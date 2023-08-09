32346f4416bd7fa04d8da19f1e4d4b922d4b2956
Thabish <kadertabish2@gmail.com>
2023-08-09 10:03:58 +0530

    csv-loaded
    Signed-off-by: Thabish <kadertabish2@gmail.com>

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,80 @@
-console.log('Welcome to the LangChain.js tutorial by LangChainers.')
+import { OpenAI } from "langchain/llms/openai";
+import { RetrievalQAChain } from "langchain/chains";
+import { HNSWLib } from "langchain/vectorstores/hnswlib";
+import { OpenAIEmbeddings } from "langchain/embeddings/openai";
+import { RecursiveCharacterTextSplitter } from "langchain/text_splitter";
+
+// Loaders
+import { DirectoryLoader } from "langchain/document_loaders/fs/directory";
+import { JSONLoader } from "langchain/document_loaders/fs/json";
+import { TextLoader } from "langchain/document_loaders/fs/text";
+import { CSVLoader } from "langchain/document_loaders/fs/csv";
+import { PDFLoader } from "langchain/document_loaders/fs/pdf";
+
+import { config } from "dotenv";
+import fs from "fs";
+import { Document } from "langchain/document";
+
+config();
+
+// Read data useing doc loader
+const loader = new DirectoryLoader("./docs", {
+	".json": (path) => new JSONLoader(path),
+	".txt": (path) => new TextLoader(path),
+	".csv": (path) => new CSVLoader(path),
+	".pdf": (path) => new PDFLoader(path),
+});
+
+// See contents of docs that are being being loaded
+const docs = await loader.load();
+// console.log(docs);
+
+const normalizeDocs = (docs: Document[]) => {
+	return docs.map((doc) => {
+		if (typeof doc.pageContent === "string") {
+			return doc.pageContent;
+		} else if (Array.isArray(doc.pageContent)) {
+			// @ts-ignore
+			return doc.pageContent.join("\n");
+		}
+	});
+};
+
+const run = async (question: string) => {
+	const model = new OpenAI({ openAIApiKey: process.env.OPENAI_API_KEY });
+	let vectorStore;
+
+	if (fs.existsSync("MyVectore.index")) {
+		console.log("Vectorstore already exists");
+		vectorStore = await HNSWLib.load(
+			"MyVectore.index",
+			new OpenAIEmbeddings()
+		);
+		console.log(`Vectorstore loader -----> ${vectorStore}`);
+	} else {
+		console.log(`Vectorstore does not exist createing.....`);
+		const textSplitter = new RecursiveCharacterTextSplitter({
+			chunkSize: 1000,
+		});
+		console.log(`Text Splitted ----> ${textSplitter}`);
+		const normalizedDocs = normalizeDocs(docs);
+		const splitDocs = await textSplitter.createDocuments(normalizedDocs);
+
+		// Create vectorstore
+		vectorStore = await HNSWLib.fromDocuments(
+			splitDocs,
+			new OpenAIEmbeddings()
+		);
+		// 17. Save the vector store to the specified path
+		await vectorStore.save("MyVectore.index");
+		console.log(`Vector store created ----> ${vectorStore}`);
+	}
+
+	// RetrievalQAChain
+	const chain = RetrievalQAChain.fromLLM(model, vectorStore.asRetriever());
+	console.log("Querying...");
+	const res = await chain.call({ query: question });
+	console.log(res);
+};
+
+run("What are the emails of users who gave a review of 7 ?");
